99e729e89c13f7e0cc5e3b688deea693d88910b7
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-12-03 13:07:10 -0600

    client and server updates

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,41 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  // counts Rounds
+  $('.submit-button').on('click', submissionCounter);
+
+  // store player info
+  $('.submit-button').on('click', storeUserInput);
+}
+
+let counter = 0;
+
+function submissionCounter(){
+    console.log('increase!')
+    counter += 1;
+    $('.update-round').empty().append(`Round: ${counter}`);
+}
+
+let userInputHistory = [];
+
+function storeUserInput() {
+  roundPlayed = {
+    player1: $('.user1-input-guess').val(),
+    player2: $('.user2-input-guess').val(),
+    player3: $('.user3-input-guess').val(),
+    player4: $('.user4-input-guess').val(),
+    };
+  userInputHistory.push(roundPlayed);
+  console.log(userInputHistory);
+
+  $('inputs').val('');
+  postRequest();
+}
+
+function postRequest(){
+  $.ajax({
+      method: 'POST',
+      url: '/players',
+      data: userInputHistory,
+  })
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -14,4 +14,16 @@ app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
-})
+});
+
+// const players = require('./public/scripts/client');
+
+let players = [];
+
+app.post('/players', function(req, res) {
+    console.log('in the post request', req.body);
+
+    players.push(req.body);
+    // res.send(201);
+} );
+
